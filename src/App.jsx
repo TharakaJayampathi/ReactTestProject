2758c2119ce74dab3309205c8ae8ccafe072a9aa
Tharaka Jayampathi <TharakaJayam@brownsgroup.com>
2025-10-21 11:20:29 +0530

    Inital Commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+const Card = () => {
+  return (
+    <div>
+      <h2>Functional Arrow Component</h2>
+    </div>  
+  )
+}
+
+const App = () => {
+  return (
+    <div>
+      <h2>Functional Arrow Component</h2>
+
+      <Card />
+      <Card />
+      <Card />
+
+    </div>  
+  )
+}
+
+export default App
